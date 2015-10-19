a646020e1367cb0e5885d16de065787c8d8c9baf
Lisle Mose <lislemose@gmail.com>
2015-10-19 13:22:23 -0400

    Output buddyinfo and stat files periodically.

--- src/main/c/status.c
+++ src/main/c/status.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-void print_status(char* desc) {
-	FILE* fp = fopen("/proc/self/status", "r");
+void print_file(char* filename, char* file_info, char* desc) {
+	FILE* fp = fopen(filename, "r");
 	char buf[5124];
 
 	while (fgets(buf, 5124, fp) != NULL) {
-		fprintf(stderr, "PROC_STATUS\t%s\t%s", desc, buf);
+		fprintf(stderr, "%s\t%s\t%s", file_info, desc, buf);
 	}
 
 	fflush(stdout);
 
 	fclose(fp);
+}
+
+void print_status(char* desc) {
+
+	print_file("/proc/self/status", "PROC_STATUS", desc);
+	print_file("/proc/buddyinfo", "BUDDY_INFO", desc);
+	char prefix[256];
+	sprintf(prefix, "STAT:%ld", time(NULL));
+	print_file("/proc/self/stat", "STAT", desc);
 }
